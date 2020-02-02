d2a79a80dfbf8ae3d75351790db21fe7c3fa1c56
User <User@DESKTOP-N15QPAP>
2020-02-02 13:42:06 +0900

    모두의 파이썬

--- PyDevTest/EveryDataAnlysis/unit07-1.py
+++ PyDevTest/EveryDataAnlysis/unit07-1.py
@@ -1,3 +1,23 @@
 '''
 unit 07 우리 동네 인구 구조 시각화하기
 '''
+file_path = 'C:\\git\\w2ji_python\\PyDevTest\\EveryDataAnlysis\\data\\age.csv'
+import csv
+import matplotlib.pyplot as plt
+f = open(file_path)
+data = csv.reader(f)
+header = next(data)
+result = []
+for row in data:
+    if '2820000000' in row[0]  :
+        for i in row[3:]:
+            num = i.replace(',','')
+            print(num)             
+            result.append( int(num) )                    
+            #result.append( row )
+
+print(header)
+print(result)
+plt.style.use('ggplot')
+plt.plot(result)
+plt.show()
---
+++ PyDevTest/EveryDataAnlysis/unit09-1.py
@@ -0,0 +1,29 @@
+'''
+unit 09 우리동네 인구 구조를 파이 차트로 나타내기
+'''
+file_path = 'C:\\git\\w2ji_python\\PyDevTest\\EveryDataAnlysis\\data\\age.csv'
+import csv
+import matplotlib.pyplot as plt
+f = open(file_path)
+data = csv.reader(f)
+header = next(data)
+name = '신도림'
+result = []
+m = []
+f = []
+for row in data:
+    if name in row[0]  :
+        for i in range(0,101):
+            m.append( int(row[i+3].replace(',','')) )
+            f.append( -int(row[-(i+1)].replace(',','')) )
+
+f.reverse()
+
+print(header)
+
+plt.style.use('ggplot')
+plt.title(name+'지역의 인구 구조')
+plt.barh( range(101), m , label='man')
+plt.barh( range(101), f , label='famle')
+plt.legend()
+plt.show()
